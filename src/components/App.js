d4e36fa598731f384d65d4435c9cf63d58fd25d0
iiffd <sml.hng@gmail.com>
2018-04-22 13:33:36 -0700

    Install react-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import Header from './globals/Header';
 import Footer from './globals/Footer';
