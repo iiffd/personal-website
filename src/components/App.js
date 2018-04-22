485008d9ba6537bba7065f6e7e0174b56355831e
iiffd <sml.hng@gmail.com>
2018-04-21 23:01:36 -0700

    Add list to Home

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import Header from './globals/Header';
 import Footer from './globals/Footer';
+import Home from './Home';
 import './App.css';
 
 class App extends Component {
@@ -9,10 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-        {/* <header className="App-header">
-          <h1 className="App-title">Powered by React</h1>
-          <img src={logo} className="App-logo" alt="logo" />
-        </header> */}
+        <Home />
         <Footer />
       </div>
     );
---
+++ src/components/Home/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+  return (
+    <section className="row">
+      <ul>
+        <li>stuff</li>
+        <li>stuff</li>
+        <li>stuff</li>
+        <li>stuff</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Home;
