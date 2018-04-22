9246ea8a96979539ab5524ca14913ad414d049a4
iiffd <sml.hng@gmail.com>
2018-04-22 00:57:14 -0700

    Add images for project tabs

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './Home.css';
+import code from './late-night-code.jpg';
+import arcade from './arcade.jpg';
+import robot from './robot-playing-piano.jpg';
 
 const Home = () => {
   return (
@@ -9,6 +12,7 @@ const Home = () => {
         <section className="col-1-3">
           <a href="#">
             <h5>Pacman Artifical Intelligence</h5>
+            <img className="img-responsive" src={arcade} alt="arcade" />
           </a>
           <p>
             Pacman artificial intelligence learns through reinforcement learning.
@@ -17,19 +21,21 @@ const Home = () => {
 
         <section className="col-1-3">
           <a href="#">
-            <h5>Pacman Artifical Intelligence</h5>
+            <h5>Machine Learning Applications</h5>
+            <img className="img-responsive" src={robot} alt="robot-playing-piano" />
           </a>
           <p>
-            Pacman artificial intelligence learns through reinforcement learning.
+            Random face generator using Generative Adverserial Network. Simpsons script generator.
           </p>
         </section>
 
         <section className="col-1-3">
           <a href="#">
-            <h5>Pacman Artifical Intelligence</h5>
+            <h5>Web Applications</h5>
+            <img className="img-responsive" src={code} alt="late night code" />
           </a>
           <p>
-            Pacman artificial intelligence learns through reinforcement learning.
+            Web applications for a diversity of purposes.
           </p>
         </section>
       </div>
