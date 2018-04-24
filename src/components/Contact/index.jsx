bbf106ec80ece079fba5ab7d1b0eb03b684d5f51
iiffd <sml.hng@gmail.com>
2018-04-23 23:57:48 -0700

    Style contact text

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
   return (
     <section>
       <h5 className="section-title">Contact</h5>
-      <p>
+      <p className="contact-text">
         The best email to reach me at is: <a href="mailto:sml.hng@gmail.com">sml.hng@gmail.com</a>
         <br />
         The best number to reach me at is: (916) 730-6087
