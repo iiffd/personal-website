967892f786d3ce3e25094c97e50054bfde173477
iiffd <sml.hng@gmail.com>
2018-05-01 23:15:39 -0700

    Create image overlay

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -11,33 +11,39 @@ const Home = () => {
       <div className="grid">
 
         <section className="col-1-3">
-          <a className="grid-heading" href="#">
+          <div className="img-container">
             {/* <h5>Pacman Artifical Intelligence</h5> */}
+            <img className="img-tab" src={arcade} alt="arcade" />
             <div className="overlay">
               <div className="overlay-text">Hello</div>
             </div>
-            <img className="img-responsive" src={arcade} alt="arcade" />
-          </a>
+          </div>
           {/* <p>
             Pacman artificial intelligence learns through reinforcement learning.
           </p> */}
         </section>
 
         <section className="col-1-3">
-          <a className="grid-heading" href="#">
+          <div className="img-container">
             {/* <h5>Machine Learning Applications</h5> */}
-            <img className="img-responsive" src={robot} alt="robot-playing-piano" />
-          </a>
+            <img className="img-tab" src={robot} alt="robot-playing-piano" />
+            <div className="overlay">
+              <div className="overlay-text">Hello</div>
+            </div>
+          </div>
           {/* <p>
             Random face generator using Generative Adverserial Network. Simpsons script generator.
           </p> */}
         </section>
 
         <section className="col-1-3">
-          <a className="grid-heading" href="#">
+          <div className="img-container">
             {/* <h5>Web Applications</h5> */}
-            <img className="img-responsive" src={code} alt="late night code" />
-          </a>
+            <img className="img-tab" src={code} alt="late night code" />
+            <div className="overlay">
+              <div className="overlay-text">Hello</div>
+            </div>
+          </div>
           {/* <p>
             Web applications for a diversity of purposes.
           </p> */}
