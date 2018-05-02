a5349697103463daaf297026770f9295b35252d8
iiffd <sml.hng@gmail.com>
2018-05-01 23:53:40 -0700

    Make overlay text thicker

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -11,39 +11,36 @@ const Home = () => {
       <div className="grid">
 
         <section className="col-1-3">
-          <div className="img-container">
-            {/* <h5>Pacman Artifical Intelligence</h5> */}
+          <a className="img-container-link" href="#">
             <img className="img-tab" src={arcade} alt="arcade" />
-            <div className="overlay">
-              <div className="overlay-text">Hello</div>
+            <div className="overlay background-color-1">
+              <div className="overlay-text text-color-white">Pacman Artificial Intelligence</div>
             </div>
-          </div>
+          </a>
           {/* <p>
             Pacman artificial intelligence learns through reinforcement learning.
           </p> */}
         </section>
 
         <section className="col-1-3">
-          <div className="img-container">
-            {/* <h5>Machine Learning Applications</h5> */}
+          <a className="img-container-link" href="#">
             <img className="img-tab" src={robot} alt="robot-playing-piano" />
-            <div className="overlay">
-              <div className="overlay-text">Hello</div>
+            <div className="overlay background-color-2">
+              <div className="overlay-text text-color-black">Machine Learning Applications</div>
             </div>
-          </div>
+          </a>
           {/* <p>
             Random face generator using Generative Adverserial Network. Simpsons script generator.
           </p> */}
         </section>
 
         <section className="col-1-3">
-          <div className="img-container">
-            {/* <h5>Web Applications</h5> */}
+          <a className="img-container-link" href="#">
             <img className="img-tab" src={code} alt="late night code" />
-            <div className="overlay">
-              <div className="overlay-text">Hello</div>
+            <div className="overlay background-color-3">
+              <div className="overlay-text text-color-white">Web Applications</div>
             </div>
-          </div>
+          </a>
           {/* <p>
             Web applications for a diversity of purposes.
           </p> */}
